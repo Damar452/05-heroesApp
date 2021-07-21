e7868c48efb4cc2c88873646e6cc87fb3280cf25
Damar452 <damarh452@gmail.com>
2021-07-21 16:49:33 -0500

    Fin de seccion

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-login',
@@ -6,11 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
 
-  constructor() { }
+  constructor(private router: Router,
+              private authService: AuthService) { }
 
-  ngOnInit(): void {
+login(){
+
+this.authService.login().subscribe(auth => {
+  console.log(auth);
+  if (auth.id){
+    this.router.navigate(['./heroes'])
   }
+})
+// this.router.navigate(['./heroes']);
+}
+
+
+
+logout(){
+  this.authService.logout();
+  this.router.navigate(['./heroes']);
+}
 
 }
